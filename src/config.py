67a28dcbae6768110962b701a7d3d4f748757def
Adrian-Istrate-11 <anyan11tm@yahoo.com>
2025-12-16 22:35:21 +0100

    Fix Render port binding (0.0.0.0 + PORT)

--- src/config.py
+++ src/config.py
@@ -1,12 +1,12 @@
-import os 
-
+import os
 
 DBNAME = "face_tracking"
 
-#API_HOST = "localhost"
-API_HOST = os.getenv("API_HOST", "127.0.0.1")
-API_PORT = 8080
+# API (backend)
+API_HOST = os.getenv("API_HOST", "0.0.0.0")
+API_PORT = int(os.getenv("API_PORT", "8080"))
 API_URL = f"http://{API_HOST}:{API_PORT}"
 
-DASH_HOST = "127.0.0.1"
-DASH_PORT = 8050
+# DASH (frontend)
+DASH_HOST = os.getenv("DASH_HOST", "0.0.0.0")
+DASH_PORT = int(os.getenv("PORT", "8050"))
