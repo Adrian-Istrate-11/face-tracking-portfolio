16db25f1e9db08e3cbbff1a79e5f848cc6e97611
Adrian-Istrate-11 <anyan11tm@yahoo.com>
2025-12-18 21:36:47 +0100

    Fix imports in status router for Render

--- src/api/routers/status.py
+++ src/api/routers/status.py
@@ -1,18 +1,14 @@
 from fastapi import APIRouter
 
-from src.config import DBNAME
-from src.common.data_transfer_objects.api_status import APIStatusDto
+from config import DBNAME
+from common.data_transfer_objects.api_status import APIStatusDto
 
 
 router = APIRouter()
 
-@router.get("/status")  #Defines a GET HTTP route at the endpoint /status.
-#When a client sends a GET request to this endpoint, the get_api_status function will be executed.
 
+@router.get("/status")
 def get_api_status() -> APIStatusDto:
-    """
-    Return API status along with database that is used within the API
-    """
     return APIStatusDto(
         running=True,
         db_name=DBNAME
