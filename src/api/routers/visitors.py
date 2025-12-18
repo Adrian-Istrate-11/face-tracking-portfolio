886e42a17693778dea63e0aef4f084cfbbdcec8f
Adrian-Istrate-11 <anyan11tm@yahoo.com>
2025-12-18 21:49:21 +0100

    Fix visitors router imports (remove src prefix)

--- src/api/routers/visitors.py
+++ src/api/routers/visitors.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Response
-from src.common.data_transfer_objects.visitors import AddVisitorDto, VisitorDto
-from src.db.sql.queries.visitors import (
+
+from common.data_transfer_objects.visitors import AddVisitorDto, VisitorDto
+from db.sql.queries.visitors import (
     add_visitor_into_the_db,
     get_all_visitors_from_db,
     delete_visitor_by_id
@@ -10,9 +11,6 @@ router = APIRouter()
 
 @router.put("")
 def add_visitor(dto: AddVisitorDto) -> Response:
-    """
-    Adds a visitor to the database
-    """
     add_visitor_into_the_db(
         name=dto.name,
         reason=dto.reason,
@@ -23,15 +21,9 @@ def add_visitor(dto: AddVisitorDto) -> Response:
 
 @router.get("", response_model=list[VisitorDto])
 def get_all_visitors() -> list[VisitorDto]:
-    """
-    Returns all visitors from the database
-    """
     return get_all_visitors_from_db()
 
 @router.delete("/{visitor_id}")
 def delete_visitor(visitor_id: int) -> dict:
-    """
-    Deletes a visitor by ID
-    """
     delete_visitor_by_id(visitor_id)
     return {"message": "Visitor deleted successfully"}
