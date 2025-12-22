6ef98455be0dc33ab2c828385ea63de6bf843c17
Adrian-Istrate-11 <anyan11tm@yahoo.com>
2025-12-22 16:13:49 +0100

    Fix invalid import syntax in add_person callback

--- src/web/callbacks/add_person.py
+++ src/web/callbacks/add_person.py
@@ -4,7 +4,8 @@ from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 
 from src.config import API_URL
-from src.common.data_transfer_objects.<ceva> import <Dto>
+from src.common.data_transfer_objects.persons import AddPersonDto
+
 
 def register_add_person_callbacks(app: Dash) -> None:
 
